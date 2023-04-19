6520417041935dabdf4ec1283e496f82b2829d7e
Dynesshely <catrol@qq.com>
2023-04-19 16:06:03 +0800

    💾 📝 Feat, Chore: Simple window usage demo.

--- Hello.OpenTK.WindowUsage/Program.cs
+++ Hello.OpenTK.WindowUsage/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Hello.OpenTK.WindowUsage;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+Console.WriteLine("Hello, OpenTK!");
+
+using (var win = new WindowView(800, 600, "Hello.OpenTK.WindowUsage")
+{
+    KeyboardStateAction = (state, window) =>
+    {
+        if (state.IsKeyDown(Keys.Space))
+        {
+            Console.WriteLine("Space is down");
+        }
+
+        if (state.IsKeyDown(Keys.Escape))
+        {
+            window.Close();
+        }
+    }
+})
+{
+    win.Run();
+}
+
---
+++ Hello.OpenTK.WindowUsage/WindowView.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Hello.OpenTK.WindowUsage;
+
+public class WindowView : GameWindow
+{
+    public Action<FrameEventArgs>? OnUpdateFrameAction { get; set; } = null;
+
+    public Action<KeyboardState, GameWindow>? KeyboardStateAction { get; set; } = null;
+
+    public WindowView(int width, int height, string title)
+        : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+    {
+
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        OnUpdateFrameAction?.Invoke(e);
+
+        KeyboardStateAction?.Invoke(KeyboardState, this);
+    }
+}
+
